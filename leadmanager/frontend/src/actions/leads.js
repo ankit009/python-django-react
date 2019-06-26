38678489fddcdbb859a69a4c26602bd86fcffd51
Ankit Sharma <ankit.sharma@lookagency.com>
2019-06-26 16:35:06 -0600

    Updating

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
+import { createMessage } from "./messages";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
 
 //GET LEADS
 export const getLeads = () => dispatch => {
@@ -17,6 +17,7 @@ export const deleteLead = id => dispatch => {
   axios
     .delete(`/api/leads/${id}/`)
     .then(res => {
+      dispatch(createMessage({ deleteLead: "Lead Deleted" }));
       dispatch({ type: DELETE_LEAD, payload: id });
     })
     .catch(err => console.log(err));
@@ -27,7 +28,17 @@ export const addLead = lead => dispatch => {
   axios
     .post(`/api/leads/`, lead)
     .then(res => {
+      dispatch(createMessage({ addLead: "Lead Added" }));
       dispatch({ type: ADD_LEAD, payload: res.data });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const errors = {
+        msg: err.response.data,
+        status: err.response.status
+      };
+      dispatch({
+        type: GET_ERRORS,
+        payload: errors
+      });
+    });
 };
--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -2,19 +2,37 @@ import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
+import Alerts from "./layout/Alerts";
 import { Provider } from "react-redux";
+
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
 import store from "../store";
+
+// Alert Options
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.TOP_CENTER,
+  timeout: 5000,
+  offset: "30px",
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+};
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <div className="container">
-            <Dashboard />
-          </div>
-        </Fragment>
+        <AlertProvider template={AlertTemplate} {...options}>
+          <Fragment>
+            <Header />
+            <Alerts />
+            <div className="container">
+              <Dashboard />
+            </div>
+          </Fragment>
+        </AlertProvider>
       </Provider>
     );
   }
